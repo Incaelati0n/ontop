a93a3c46ccdc5701c52db681d7e10e7cd398d16a
Plaikner Daniel (Student Com15) <plaikner.daniel@gmail.com>
2021-02-28 23:58:20 +0100

    small flink fix

--- db/rdb/src/main/java/it/unibz/inf/ontop/dbschema/impl/FlinkSQLDBMetadataProvider.java
+++ db/rdb/src/main/java/it/unibz/inf/ontop/dbschema/impl/FlinkSQLDBMetadataProvider.java
@@ -4,14 +4,13 @@ import com.google.inject.assistedinject.Assisted;
 import com.google.inject.assistedinject.AssistedInject;
 import it.unibz.inf.ontop.dbschema.MetadataLookup;
 import it.unibz.inf.ontop.dbschema.NamedRelationDefinition;
-import it.unibz.inf.ontop.dbschema.RelationID;
 import it.unibz.inf.ontop.exception.MetadataExtractionException;
 import it.unibz.inf.ontop.injection.CoreSingletons;
 
 import java.sql.Connection;
 import java.sql.SQLException;
 
-public class FlinkSQLDBMetadataProvider extends DefaultSchemaDBMetadataProvider{
+public class FlinkSQLDBMetadataProvider extends DefaultSchemaCatalogDBMetadataProvider{
 
     @AssistedInject
     FlinkSQLDBMetadataProvider(@Assisted Connection connection, CoreSingletons coreSingletons) throws MetadataExtractionException {
@@ -19,19 +18,10 @@ public class FlinkSQLDBMetadataProvider extends DefaultSchemaDBMetadataProvider{
     }
 
     @Override
-    protected String getRelationCatalog(RelationID id) { return "default_catalog"; }
-
-    @Override
-    protected String getRelationSchema(RelationID id) { return "default_database"; }
-
-    @Override
-    protected String getRelationName(RelationID id) { return "test"; }
-
-    @Override
     public void insertIntegrityConstraints(NamedRelationDefinition relation, MetadataLookup metadataLookup) throws MetadataExtractionException {
         try {
             insertPrimaryKey(relation);
-            /* try and get to work */
+            /* not (yet) available in flink */
             //insertUniqueAttributes(relation);
             //insertForeignKeys(relation, metadataLookup);
         }
